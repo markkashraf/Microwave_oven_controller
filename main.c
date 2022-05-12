a164a5cb2e12cbdf491fb5e5141494ddc286c778
MohammadDallash <105324962+MohammadDallash@users.noreply.github.com>
2022-05-12 17:56:25 +0200

    Creating a new struct with states that require a parameter when entering it and testing that

--- main.c
+++ main.c
@@ -18,26 +18,57 @@ void State_A_Output()
      printf("State_A_output\n");
 }
 
+
+
+void State_B_Enter(int x)
+{
+     printf("State_B_Entered and the enter Parameter is %i \n", x);
+}
+
+
+void State_B_Loop()
+{
+     printf("State_B_looping\n");
+}
+
+
+void State_B_Output()
+{
+     printf("State_B_output\n");
+}
+
+
+
 typedef void (*f_pointer_void_void)(void);
+typedef void (*f_pointer_void_int)(int);
 
 
-typedef struct StateStruct
+typedef struct StateNoParameters
 {
     f_pointer_void_void Enter;
     f_pointer_void_void Loop;
     f_pointer_void_void Output;
-} StateStruct;
+} StateNoParameters;
+
+typedef struct StateOneEnterParameter
+{
+    f_pointer_void_int Enter;
+    f_pointer_void_void Loop;
+    f_pointer_void_void Output;
+} StateOneEnterParameter;
  
-
-
+ 
 
 
 int main()
 {
     
     
-    StateStruct  State_A = {State_A_Enter , State_A_Loop, State_A_Output};
+    StateNoParameters  State_A = {State_A_Enter , State_A_Loop, State_A_Output};
     State_A.Enter();
+    
+    StateOneEnterParameter  State_B = {State_B_Enter , State_B_Loop, State_B_Output};
+    State_B.Enter(5);
     
     
 
