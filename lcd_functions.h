250d97fe6bbe5991a5f5280faec29d11c84fe771
markkashraf <54954479+markkashraf@users.noreply.github.com>
2022-05-12 23:52:04 +0200

    Add files via upload

---
+++ lcd_functions.h
@@ -0,0 +1,19 @@
+#define RS 0x01				 	//RS -> PB0 (0x01)
+#define EN 0x04  		 	 	//EN -> PB2 (0x04)
+
+void LCD_Write4bits(unsigned char data, unsigned char control);
+
+void LCD4bits_Data(unsigned char data);
+
+void LCD4bits_Cmd(unsigned char command);
+
+void LCD4bits_Init(void);
+
+void LCD_WriteString(char* str);
+
+void Cursor_Left(int n);
+
+void Cursor_Right(int n);
+
+
+	
