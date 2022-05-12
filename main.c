c495e8d0ddf4d0d2b604b1b740abfef2b841eefb
MohammadDallash <105324962+MohammadDallash@users.noreply.github.com>
2022-05-12 13:49:01 +0200

    creating a new type

--- main.c
+++ main.c
@@ -6,10 +6,16 @@ void funMyfun()
 }
 
 
+typedef void (*f_pointer_void_void)(void);
+
+/*This creates a type, named f_pointer_void_void for a pointer to a function that takes a void argument and returns nothing, 
+which matches the signature of the functions we will need
+
+*/
 int main()
 {
     
-   void (*p) () = funMyfun;
+   f_pointer_void_void p  = funMyfun;
    (*p) ();
 
     return 0;
