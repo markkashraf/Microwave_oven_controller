7823f590f69d1ee7041109d36ce6c759e329d78e
MohammadDallash <105324962+MohammadDallash@users.noreply.github.com>
2022-05-12 13:41:26 +0200

    testing pointer to function

--- main.c
+++ main.c
@@ -1,1 +1,16 @@
-//to be edited later
+#include <stdio.h>
+
+void funMyfun()
+{
+     printf("Hello World");
+}
+
+
+int main()
+{
+    
+   void (*p) () = funMyfun;
+   (*p) ();
+
+    return 0;
+}
