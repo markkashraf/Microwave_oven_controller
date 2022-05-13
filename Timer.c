e658aa6801aa9bd573451cac5f6df1984ec921f5
markkashraf <54954479+markkashraf@users.noreply.github.com>
2022-05-13 22:56:05 +0200

    Update Timer.c

--- Timer.c
+++ Timer.c
@@ -3,9 +3,35 @@
 
 void Timer_Enter(int minutes ,int seconds)
 {
-    myStates.Timer.minutes = minutes;
-    myStates.Timer.seconds = seconds;
-    CurrentState = Timer;
+CurrentState = Timer;
+int i , j;
+
+for(i = minutes; i>=0; i--)
+{
+    
+for(j = seconds; j>=0; j--)
+{
+    
+    LCD4bits_Data((i/10)+'0');// Send second digit in minutes
+
+    LCD4bits_Data((i%10)+'0'); // Send first digit in minutes
+
+    LCD4bits_Data(':');
+
+    LCD4bits_Data((j/10)+'0'); // Send second digit in seconds
+
+    LCD4bits_Data((j%10)+'0'); // Send first digit in seconds
+
+    systick_delay_msec(1000); //wait 1 sec
+    
+    LCD4bits_Cmd(lcd_clear); // clear screen
+
+    }
+    seconds = 59; //after first second all minutes are 60 seconds
+
+    }
+    
+   
     
 }
 
