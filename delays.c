408fbc363db818384f893c343172ccc842059abb
hossamadel7 <57440281+hossamadel7@users.noreply.github.com>
2022-05-13 19:52:30 +0200

    Update delays.c

--- delays.c
+++ delays.c
@@ -4,23 +4,23 @@
 //extra delay functions
 
 void delay_micro(int n){
-int i,j;
-for(i=0;i<n;i++)
-for(j=0;j<3;j++)
-{}
+      int i,j;
+      for(i=0;i<n;i++)
+      for(j=0;j<3;j++)
+
 }
 
 //systick delay functions
 void SysTick_Wait(unsigned long delay) // delay = number of ticks
 {
-NVIC_ST_CTRL_R = 0X00;
-NVIC_ST_RELOAD_R = delay - 1;
-NVIC_ST_CURRENT_R = 0;
-NVIC_ST_CTRL_R = 0X05; // ENABLE TIMER AND CLK SRC
-while((NVIC_ST_CTRL_R&0x00010000)==0) 
-{
-//wait for count flag
-}
+      NVIC_ST_CTRL_R = 0X00;
+      NVIC_ST_RELOAD_R = delay - 1;
+      NVIC_ST_CURRENT_R = 0;
+      NVIC_ST_CTRL_R = 0X05; // ENABLE TIMER AND CLK SRC
+      while((NVIC_ST_CTRL_R&0x00010000)==0) 
+      {
+      //wait for count flag
+      }
 }
 
 void SysTick_Wait1ms()
