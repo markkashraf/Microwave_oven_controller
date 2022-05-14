a54a95cc30fe5f7bd619fb91181a01e9189e8350
hossamadel7 <57440281+hossamadel7@users.noreply.github.com>
2022-05-14 22:11:23 +0200

    Update Popcorn.c

--- Popcorn.c
+++ Popcorn.c
@@ -4,19 +4,21 @@
 void Popcorn_Enter()
 {
      CurrentState = Popcorn;
-     printf("Popcorn_Entered and the enter Parameter is \n");
-}
+	  LCD4bits_Cmd(lcd_clear);      //LCD clears
+     
+	 LCD_WriteString("Popcorn      (1min) ");
 
 
 void Popcorn_Loop()
 {
-     printf("Popcorn_looping\n");
+   if (GPIO_PORTF_DATA_R & 0x10 == 0)
+		        myStates.Timer.Enter(1,1)	
 }
 
 
 void Popcorn_Output()
 {
-     printf("Popcorn_output\n");
+
 }
 
 void Popcorn_Init()
