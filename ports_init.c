0a3cd1de5139b629a5f5d07e81e7ddcd8cde82a4
marcosherif <68437948+marcosherif@users.noreply.github.com>
2022-05-14 22:23:25 +0200

    Update ports_init.c

--- ports_init.c
+++ ports_init.c
@@ -50,12 +50,12 @@ void SW2_Init(void)
 void PortA_init(void){         
 	SYSCTL_RCGCGPIO_R |= 0x01;
 	while((SYSCTL_PRGPIO_R &= 0x01) == 0);
-	GPIO_PORTA_AMSEL_R &= ~0x1C;  
-	GPIO_PORTA_PCTL_R &= ~0x000FFF00;
-	GPIO_PORTA_AFSEL_R &= ~0x1C;
-	GPIO_PORTA_DIR_R |= 0x1C;
-	GPIO_PORTA_DEN_R |= 0x1C;
-	GPIO_PORTA_DATA_R &= ~0x1C;	
+	GPIO_PORTA_AMSEL_R &= ~0x02;  
+	GPIO_PORTA_PCTL_R &= ~0x000000F0;
+	GPIO_PORTA_AFSEL_R &= ~0x02;
+	GPIO_PORTA_DIR_R |= 0x02;
+	GPIO_PORTA_DEN_R |= 0x02;
+	GPIO_PORTA_DATA_R &= ~0x02;	
 }
 
 void PortB_init(void){        
