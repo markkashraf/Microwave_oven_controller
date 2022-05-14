54364105e521302cb44722edea730ff4ce831a20
mohamed ahmed samir <105310173+Mohamed-samir-504@users.noreply.github.com>
2022-05-14 10:34:45 +0200

    update header file with changes

--- ports_init.h
+++ ports_init.h
@@ -1,7 +1,12 @@
-void Leds_init(void);
+void PortF_init(void);
 void PortA_init(void);
 void PortB_init(void);
 void PortC_init(void);
 void PortE_init(void);
-void SW2_Init(void);
-void SW1_Init(void);
+unsigned char Switch1_input(void);
+unsigned char Switch2_input(void);
+void Output_on_leds(unsigned char data);
+
+#define red 0x02;
+#define blue 0x04;
+#define green 0x08;
