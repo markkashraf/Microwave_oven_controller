d9416d7a71ceacb52a982058450f98d9b4021529
MohammadDallash <105324962+MohammadDallash@users.noreply.github.com>
2022-05-15 13:52:30 +0200

    Update Pause.c

--- Pause.c
+++ Pause.c
@@ -1,24 +1,46 @@
 #include "Pause.h"
 #include "FSMmanager.h"
+#include "lcd_functions.h"
+#include "delays.h"
+#include "ports_init.h"
+#include "tm4c123gh6pm.h"
 
-void Pause_Enter(int parameter)
+
+void Pause_Enter(int minutes ,int seconds)
 {
-    myStates.Pause .parameter = parameter;
+    myStates.Pause.minutes = minutes;
+    myStates.Pause.seconds = seconds;
     CurrentState = Pause;
-    
+    LCD4bits_Cmd(0x01);
+    myStates.Pause.Output();
 }
 
 
 void Pause_Loop()
-{
-     
-    printf("Pause_looping\n");
+{ 
+    if (Switch2_input() == 0)
+    {
+        myStates.Timer.Enter(myStates.Pause.minutes, myStates.Pause.seconds);
+        return;
+    }
+
+    if (Switch1_input() == 0)
+    {
+        myStates.Idle.Enter();
+        return;
+    }
 }
 
 
 void Pause_Output()
 {
-     printf("Pause_output\n");
+    LCD4bits_Data((myStates.Pause.minutes/10)+'0');// Send second digit in minutes
+
+    LCD4bits_Data((myStates.Pause.minutes%10)+'0'); // Send first digit in minutes
+
+    LCD4bits_Data(':');
+    LCD4bits_Data((myStates.Pause.seconds/10)+'0'); // Send second digit in seconds
+    LCD4bits_Data((myStates.Pause.seconds%10)+'0'); // Send first digit in seconds
 }
 
 void Pause_Init()
