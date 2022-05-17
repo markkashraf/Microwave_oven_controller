706cc428f1efb7a2d891ff473f1980d96342f22b
mohamed ahmed samir <105310173+Mohamed-samir-504@users.noreply.github.com>
2022-05-17 19:01:32 +0200

    Update Pause.h

--- Pause.h
+++ Pause.h
@@ -1,7 +1,6 @@
 #ifndef HEADER_H_NAME
 #define HEADER_H_NAME
-void Pause_Enter(int parameter);
-void Pause_Loop();
+void Pause_Enter(int parameter1, int parameter2);
 void Pause_Output();
 void Pause_Init();
 #endif
