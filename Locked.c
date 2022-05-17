0a6fcc2de71f293141aa92715f37a2c8697c630c
marcosherif <68437948+marcosherif@users.noreply.github.com>
2022-05-17 04:33:05 +0200

    Update Locked.c
    create child lock functionality for microwave

--- Locked.c
+++ Locked.c
@@ -5,16 +5,65 @@
 #include "lcd_functions.h"
 #include "delays.h"
 
+int sum,iterator1,iterator2;
+char x;
+int iterator;
+int PasswordFromUser[4];
 
 
 void Locked_Enter(int* arry)
 {
 
+	LCD4bits_Cmd(0x01);
+	systick_delay_msec(500);
+        CurrentState = Locked;
+        sum = 0;
+        iterator = 0;
+	myStates.Locked.PassWord = arry;
+	for(iterator1 = 0; iterator1<4; iterator1++)PasswordFromUser[iterator1] = 0;
+        myStates.Locked.Output();
+	systick_delay_msec(700);
 }
 
 
 void Locked_Loop()
 {
+	x = get_keypad_input();
+	if(x == 'z')
+	{
+		myStates.Locked.Enter(myStates.Locked.PassWord);
+		return;
+	}
+	if(iterator <=3)
+    	{
+		LCD4bits_Data('*');
+		PasswordFromUser[iterator] = x - '0';
+	}
+    	iterator ++;
+
+    	if (iterator > 4 && x == 'x')
+    	{
+        	for (iterator2 = 0; iterator2<4; iterator2++) sum += (PasswordFromUser[iterator2] == myStates.Locked.PassWord[iterator2]) ? 1:0;
+
+		if (sum==4)
+		{
+		    LCD4bits_Cmd(0x01);
+		    LCD_WriteString("Child lock off");
+		    systick_delay_msec(2000);
+		    LCD4bits_Cmd(0x01);
+		    myStates.Idle.Enter();
+		}
+		else
+		{
+		    LCD4bits_Cmd(0x01);
+		    LCD_WriteString("WRONG!!!!!!");
+		    systick_delay_msec(2000);
+		    LCD4bits_Cmd(0x01);
+		    myStates.Locked.Enter(myStates.Locked.PassWord);
+		}
+
+        	return;
+   	 }
 		
     
 
@@ -23,6 +72,9 @@ void Locked_Loop()
 
 void Locked_Output()
 {
+	LCD_WriteString("Child lock on");
+    	LCD4bits_Cmd(0xC0);
+    	LCD_WriteString("Password:  ");
 }
 
 void Locked_Init()
